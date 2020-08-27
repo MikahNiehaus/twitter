da60373a66ba711155e0b9f53f0ff1f87b7ea1ba
Mikah Niehaus <Graywolf.niehaus@live.com>
2020-08-27 12:23:39 -0500

    test

--- frontend/src/containers/App.js
+++ frontend/src/containers/App.js
@@ -1,12 +1,10 @@
-import React from 'react';
+// import React from 'react';
 import './App.css';
 import React, { Component } from 'react';
-import {
-  BrowserRouter as Router,
-  Route
-} from 'react-router-dom';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
 import NavBar from '../components/NavBar';
 import MoviesPage from './MoviesPage';
+
 
 class App extends Component {
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,18 +1,34 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './containers/App';
+// import * as serviceWorker from './serviceWorker';
+
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+// // If you want your app to work offline and load faster, you can change
+// // unregister() to register() below. Note this comes with some pitfalls.
+// // Learn more about service workers: https://bit.ly/CRA-PWA
+// serviceWorker.unregister();
+
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import { render } from 'react-dom';
 import App from './containers/App';
-import * as serviceWorker from './serviceWorker';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import catsReducer from './reducers/catsReducer.js';
+import { Provider } from 'react-redux';
 
-
-ReactDOM.render(
-  <React.StrictMode>
+const store = createStore(catsReducer, applyMiddleware(thunk))
+render(
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
